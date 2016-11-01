eeab37f359283c7b91525e6d0b77778ff06a6636
Anderson Meira <andersonmeira@outlook.com>
2016-11-01 15:44:09 -0200

    criação dos componentes

---
+++ app/components/GitHub.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var SearchUser = require('./SearchUser');
+
+var GitHub = React.createClass({
+
+    render: function() {
+        return(
+            <div className="container">
+                <SearchUser />
+            </div>
+        );
+    }
+
+});
+
+module.exports = GitHub;
